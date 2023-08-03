27226d7492336f407e199d0ab27fe41b7b541104
datumcoder <datumcoder@gmail.com>
2023-08-03 23:19:38 +0200

    added mobile nav plus buttons for mobile added my page and nav buttons also title text and social icons

---
+++ src/components/Main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Image from '../images/main_bg.jpg'
+import { TypeAnimation } from 'react-type-animation'
+import {FaLinkedin,FaTwitter,FaGithub,FaWhatsapp} from 'react-icons/fa'
+
+
+
+const Main = () => {
+  return (
+
+    <div id='main'>
+        <img className='w-full h-screen object-cover object-center scale-x-[-1]' src={Image} alt=""/>
+    <div className="w-full h-screen absolute top-0 right-0 bg-white/50">
+
+    <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center'>
+      <h1 className='sm:text-5xl text-4xl font-bold text-black'>I'm Patrick O'Sullivan</h1>
+      <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-900'>I'm a 
+      <TypeAnimation
+      sequence={[
+        
+        'Developer',
+        2000,
+        ' Coder',
+        2000,
+        'Tech Enthusiast',
+        3000,
+      
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+      style={{fontSize:'1em', paddingLeft:'5px' }}
+    />
+    </h2>
+    <div className='flex justify-between pt-6 max-w-[200px] w-full'>
+      <FaGithub className='cursor-pointer ' size={30} />
+      <FaLinkedin className='cursor-pointer' size={30}/>
+      <FaTwitter className='cursor-pointer' size={30}/>
+      <FaWhatsapp className='cursor-pointer' size={30}/>
+      </div>
+      
+    </div>
+    </div>
+
+
+
+    </div>
+
+  )
+}
+
+export default Main
+
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react'
-import {AiOutlineHome, AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineHome, AiOutlineMenu,AiOutlineProject,AiOutlineMail} from 'react-icons/ai'
+
+import {BsPerson} from 'react-icons/bs'
+import {GrProjects} from 'react-icons/gr'
+
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
@@ -14,20 +18,58 @@ const Sidenav = () => {
       {
         nav ? (
           <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-          <a href="#main" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+          <a href="#main" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-red-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
             <AiOutlineHome size={20}/>
             <span className='pl-4'>Home</span>
+          </a>
+          <a href="#skills" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+            <GrProjects size={20}/>
+            <span className='pl-4'>Skills</span>
+          </a>
+          <a href="#projects" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+            <AiOutlineProject size={20}/>
+            <span className='pl-4'>Projects</span>
+          </a>
+          <a href="#main" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+            <BsPerson size={20}/>
+            <span className='pl-4'>Resume</span>
+          </a>
+          <a href="#contact" className=' w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+            <AiOutlineMail size={20}/>
+            <span className='pl-4'>Contact</span>
           </a>
           
           </div>
         )
       :  (
-        <div></div>
-        )
-      }
-      </div> 
+          ""
+        )}
 
-  );
+
+          <div className='md:block hidden fixed top-[25%] z-10'>
+            <div className='flex flex-col'>
+              <a href="main" className='rounded-full shadow-lg bg-white shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineHome size={20}/>
+              </a>
+              <a href="skils" className='rounded-full shadow-lg   bg-white  shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <GrProjects size={20}/>
+              </a>
+              <a href="projects" className='rounded-full shadow-lg  bg-white  shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineProject size={20}/>
+              </a>
+              <a href="Resume" className='rounded-full shadow-lg  bg-white  shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <BsPerson size={20}/>
+              </a>
+              <a href="contact" className='rounded-full shadow-lg  bg-white  shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail size={20}/>
+              </a>
+            </div>
+          </div>
+
+
+
+      </div> 
+      );
     };
   
 
