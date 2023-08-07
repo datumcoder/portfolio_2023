d965bb23dd1a2d42021e2b4f8c7e941667e32508
datumcoder <datumcoder@gmail.com>
2023-08-07 17:17:17 +0200

    Updated project text so ovelay was in the right position and main text was resized to suit diffrent screens

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,8 +13,8 @@ const Main = () => {
     <div className="w-full h-screen absolute top-0 right-0 bg-white/50">
 
     <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center'>
-      <h1 className='sm:text-5xl text-4xl font-bold text-[#001b5e]'>I'm Patrick O'Sullivan</h1>
-      <h2 className='flex sm:text-3xl text-2xl pt-4  text-[#001b5e]'>I'm a  
+      <h1 className=' sm:text-4xl text base font-bold text-center text-[#001b5e]'>I'm Patrick O'Sullivan</h1>
+      <h2 className='flex sm:text-4xl md:text-4xl pt-4  text-[#001b5e]'>I'm a  
       <TypeAnimation
       sequence={[
         
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,22 +8,22 @@ import webdevImg from '../images/webdev.png'
 
 const Projects = () => {
   return (
-    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+    <div id='projects' className='max-w-[1080px] m-auto md:pl-20 p-4 py-16'>
 
     <h1 className=' text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
 
     <p className='text-center py-8'>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos consequatur voluptatum at eum id aliquam ut illo quidem repudiandae possimus, explicabo praesentium aspernatur? Earum quo voluptatibus dicta laborum obcaecati perspiciatis.
     </p>
-      <div className='grid sm:grid-cols-2 gap-12'>
 
-      <a href="https://brochow.school" target='blank'><ProjectItem img={schoolImg} title='School Website'/></a> 
-      <a href="https://talknative.pl" target='blank'> <ProjectItem img={freelancerImg} title='Freelancer Website'/></a> 
-     <a href="https://patrickosullivan.dev" target='blank'><ProjectItem img={webdevImg} title='Web Developer Website'/></a> 
-     <a href="https://mystifying-yalow-521b6e.netlify.app/" target='blank'><ProjectItem img={websiteImg} title='Beautician Website'/></a> 
-      
-      
+    <div>   <div className='grid sm:grid-cols-2 gap-14'>
+      <a href="https://brochow.school" target='blank'><ProjectItem img={schoolImg} title=''/></a> 
+      <a href="https://talknative.pl" target='blank'> <ProjectItem img={freelancerImg} title=''/></a> 
+      <a href="https://patrickosullivan.dev" target='blank'><ProjectItem img={webdevImg} title=''/></a> 
+      <a href="https://mystifying-yalow-521b6e.netlify.app/" target='blank'><ProjectItem img={websiteImg} title=''/></a> 
       </div>
+      </div>
+   
     </div>
   )
 }
