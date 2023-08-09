0744317b2e4a3fac1d9ea887db6a6d604bff91a9
datumcoder <datumcoder@gmail.com>
2023-08-09 14:43:24 +0200

    added form to index.html with honey-pot

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,21 +10,21 @@ const Contact = () => {
   <div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
     <h1 class="block w-full text-center text-grey-darkest mb-6"></h1>
 
-    <form id='contact' class="mb-4 md:flex md:flex-wrap md:justify-between" 
-    name="contact" method="POST" data-netlify="true" onSubmit="submit">
+    <form id='contact' class="mb-4 md:flex md:flex-wrap md:justify-between" name="contact" method="POST">
+   
       <input type="hidden" name="form-name" value="contact" />
       <div class="flex flex-col mb-4 md:w-full">
         <label class="mb-2 uppercase tracking-widest font-bold text-lg text-grey-darkest" htmlFor="first_name">Your Name</label>
-        <input class="border py-2 px-3 text-grey-darkest md:mr-2" type="text" name="name" id="name"/>
+        <input required class="border py-2 px-3 text-grey-darkest md:mr-2" type="text" name="name" id="name"/>
       </div>
     
       <div class="flex flex-col mb-4 md:w-full">
         <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" htmlFor="email">Email</label>
-        <input class="border py-2 px-3 text-grey-darkest" type="email" id="inputEmail" name="email" />
+        <input required class="border py-2 px-3 text-grey-darkest" type="email" id="inputEmail" name="email" />
       </div>
       <div class="flex flex-col mb-6 md:w-full">
         <label className='uppercase text-sm py-2 font-bold'>Message</label>
-            <textarea className='border-2 rounded-lg p-4' rows="7"  name='message'/>
+            <textarea required className='border-2 rounded-lg p-4' rows="7"  name='message'/>
       </div>
       <button class="bg-[#001b5e] text-white uppercase text-lg mx-auto p-4 rounded" type="submit">Submit</button>
   
