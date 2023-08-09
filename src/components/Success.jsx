01eed34f059934e180221832d5c6fcc41f326c5d
datumcoder <datumcoder@gmail.com>
2023-08-09 17:49:43 +0200

    Success component added with correct info

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Submitted  = () => {
+const Success  = () => {
   return (
     <div>
        <div className='max-w[1040px] m-auto md:pl-20 p4 py-16'>
@@ -8,13 +8,11 @@ const Submitted  = () => {
       Contact
       </h1>
       <div class="flex items-center h-screen w-full bg-gray-100">
-    <div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+  <div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
     <h1 class="block w-full text-center text-grey-darkest mb-6"></h1>
 
-  
 
     <h1 className='text-bold'>Thank you! Your submission has been received.</h1>
-   
   
     </div>
   </div>
@@ -23,4 +21,4 @@ const Submitted  = () => {
   )
 }
 
-export default Submitted 
+export default Success 
